857d578bc78163115e294502fbf89d2bc3d6f16d
CDKML <kamalloco@hotmail.com>
2022-11-05 19:07:13 +0100

    - Implement sorting logic

--- Controllers/TextController.cs
+++ Controllers/TextController.cs
@@ -1,42 +1,83 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Text_Processor_Web_API_Rest.Models;
+using Text_Processor_Web_API_Rest.ViewModel;
 
-namespace Text_Processor_Web_API_Rest_Rest.Controllers
+namespace Text_Processor_Web_API_Rest.Controllers
 {
     public class TextController : Controller
     {
         // GET: TextController
-        public ActionResult Index()
+        public IActionResult Index(TextVM model)
         {
-            return View();
+            PopulateDropdownList(model);
+            return View(model);
+        }
+
+        private static void PopulateDropdownList(TextVM model)
+        {
+            model.SortingOptionsList = SortingOptions.GetAll();
         }
 
         // GET: TextController/OrderOptions
-        public ActionResult OrderOptions()
+        public IActionResult OrderOptions(TextVM model)
         {
+            model.SortingOptionsList = SortingOptions.GetAll();
+
             //Deliver the list of Order Options
             return View();
         }
 
-        // GET: TextController/OrderedText
-        public ActionResult OrderedText()
+        // POST: TextController/OrderText
+        [HttpPost]
+        public IActionResult OrderText(TextVM model)
         {
+            model.SortingOptionsList = SortingOptions.GetAll();
+
+            var selectedOption = model.SelectedSortingOption;
+            List<string> ListString = model.TextToSort.Split(' ').ToList();
+            var sortedList = new List<string>();
+            sortedList = SortText(selectedOption, ListString);
+
+            model.SortedText = String.Join(" ", sortedList.ToArray());
             //Should recover all words of the string and sort them based on the received sort option
             //there are 3 types of sorting (AlphabeticAsc, AlphabeticDesc, LenghtAsc)
             //Should deliver a list of ordered words as a result
-            return View();
+            return View(nameof(Index), model);
         }
 
-        // GET: TextController/Statistics
-        public ActionResult Statistics(string textToAnalyze)
+        private static List<string> SortText(string selectedOption, List<string> ListString)
+        {
+            List<string> sortedList;
+            if (selectedOption == SortingOptions.SortingOptionType.AlphabeticAsc.ToString())
+            {
+                sortedList = ListString.OrderBy(x => x).ToList();
+            }
+            else if (selectedOption == SortingOptions.SortingOptionType.AlphabeticDesc.ToString())
+            {
+                sortedList = ListString.OrderByDescending(x => x).ToList();
+            }
+            else if (selectedOption == SortingOptions.SortingOptionType.LengthAsc.ToString())
+            {
+                sortedList = ListString.OrderBy(x => x.Length).ToList();
+            }
+            else
+            {
+                sortedList = ListString;
+            }
+
+            return sortedList;
+        }
+
+        // POST: TextController/AnalyzeText
+        [HttpPost]
+        public IActionResult AnalyzeText(TextVM model)
         {
             //Returning a json complex POCO object called TextStatistics with text statistics
             //of hyphens quantity, word quantity, spaces quantity
-            return View();
+            return View(nameof(Index), model);
         }
     }
 }
---
+++ Models/SortingOptionType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Text_Processor_Web_API_Rest.Models
+{
+    public static class SortingOptions
+    {
+        public enum SortingOptionType
+        {
+            AlphabeticAsc,
+            AlphabeticDesc,
+            LengthAsc
+        }
+
+        public static List<SelectListItem> GetAll()
+        {
+             return new SelectList(Enum.GetValues(typeof(SortingOptionType))).ToList();
+        }
+    }
+}
