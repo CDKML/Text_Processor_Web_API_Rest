ea65a1fe23df0b02b8e4567371feba60616091a9
CDKML <kamalloco@hotmail.com>
2022-11-07 18:50:02 +0100

    - Add sentence analyzer - Rename TexVM class

---
+++ Models/AnalyzeText.cs
@@ -0,0 +1,9 @@
+﻿namespace Text_Processor_Web_API_Rest.Models
+{
+    public class AnalyzeText
+    {
+        public int words { get; set; }
+        public int spaces { get; set; }
+        public int hyphens { get; set; }
+    }
+}
